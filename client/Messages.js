a0181134fd0058748631e2fccdfe3a83127cc34a
KMTaylor023 <kmtaylor023@gmail.com>
2018-04-08 23:11:28 -0400

    added over loby code i've used in lobby.js, put socket in client.js, added lobby html, added Messages construct

---
+++ client/Messages.js
@@ -0,0 +1,20 @@
+//Holds all message names to and from server
+//Meaning: C_: to client, H_: to host, S_: to server
+const Messages = Object.freeze({
+  //Client messages
+  C_Update_Lobby: 'c_updateLobby',
+  C_Error: 'c_err',
+  C_Currency_Click: 'c_currencyClick',
+  C_Attack_Click: 'c_attackClick',
+  //Host messages
+  H_Player_Joined: 'h_addPlayer',
+  H_Player_Left: 'h_removePlayer',
+  H_Currency_Click: 'h_currencyClick',
+  H_Attack_Click: 'h_attackClick',
+  //Server messages
+  S_Create_Room: 's_createRoom',
+  S_Disconnect: 'disconnect',
+  S_Join: 'join',
+});
+
+modules.exports.Messages = Messages;
