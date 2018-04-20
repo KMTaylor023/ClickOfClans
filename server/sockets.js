7f218a263a2ae8eb46a5f99c6b3f779fce9e16c4
KMTaylor023 <kmtaylor023@gmail.com>
2018-04-19 22:48:33 -0400

    fixed an error that prevented me from running

--- server/sockets.js
+++ server/sockets.js
@@ -268,7 +268,7 @@ const setupSockets = (ioServer) => {
         data.targetHash,
         data.x,
         data.y,
-        data.color,
+        data.color
       );
 
       // send the target data to the host
