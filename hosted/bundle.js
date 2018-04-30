b0cfc9fc5715af330f3a74d33c4f54462c5f2cce
Jeffrey Karger <whichazelnj13@aol.com>
2018-04-30 15:54:26 -0400

    add webfont

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -385,7 +385,7 @@ var redraw = function redraw() {
         ctx.fillStyle = "white";
         ctx.fillRect(300, 200, 100, 40);
         ctx.fillStyle = "black";
-        ctx.font = "30px Arial";
+        ctx.font = "30px Do Hyeon";
         ctx.fillText("The winner is player " + winnerNum, 300, 230, 100);
         ctx.restore();
 
